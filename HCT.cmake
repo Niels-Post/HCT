90b3c1829b6663e55fbf171fff14f3c8bdeba067
Niels Post <niels.maff.pc@gmail.com>
2019-10-02 12:34:05 +0200

    - Added target autoloader - Removed dependency on a second target for building

--- HCT.cmake
+++ HCT.cmake
@@ -2,5 +2,27 @@ function(add_hwlib_bmptk_target name path makefile)
     set(name ${name})
     set(path ${path})
     set(makefile ${makefile})
-    add_subdirectory("${HCT}/Target_Maker" "${CMAKE_CURRENT_BINARY_DIR}/HCT/${name}")
+    add_subdirectory("$ENV{HCT}/Target_Maker" "${CMAKE_CURRENT_BINARY_DIR}/HCT/${name}" EXCLUDE_FROM_ALL)
+endfunction()
+
+
+function(autoload_hwlib_bmptk_targets directory)
+    file(GLOB_RECURSE makefiles
+            "${directory}/*Makefile")
+    foreach (makefile ${makefiles})
+        get_filename_component(makefile_name "${makefile}" NAME)
+        get_filename_component(makefile_directorypath "${makefile}" DIRECTORY)
+        get_filename_component(makefile_directoryname "${makefile_directorypath}" NAME)
+
+        file(READ ${makefile} makefile_contents)
+        string(FIND "${makefile_contents}" "include $(HWLIB)" is_hwlib_makefile)
+
+        if (${is_hwlib_makefile} EQUAL -1)
+            continue()
+        endif ()
+
+        message("\nMakefile found in: ${makefile_directorypath}")
+        add_hwlib_bmptk_target("${makefile_directoryname}" "${makefile_directorypath}" "${makefile_name}")
+    endforeach ()
+
 endfunction()
